70a857982bd932d5ffb77a01b90a7135d1c7c587
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-03-05 10:16:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Your code here.
+const container = document.getElementsByClassName('items');
+let isDragging = false;
+let startX,startY,startScrollX;
+
+container.addEventListener('mousedown' ,(e)=>{
+	isDragging= true;
+	startX = e.clientX;
+	startY = e.clientY;
+	startScrollX = container.scrollLeft;
+});
+container.addEventListener('mousemove',(e)=>{
+	if(!isDragging) return;
+	const distanceX = e.clientX - startX;
+	container.scrollLeft = startScrollX - distanceX;
+});
+container.addEventListener('mouseup' ,(e)=>{
+	isDragging=false;
+});
